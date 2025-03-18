b72b0ffb5755303ea4bb35a9b6e8e95c9f1bcbee
luisma-2601 <142758332+luisma-2601@users.noreply.github.com>
2025-03-18 15:39:47 -0400

    xx

--- src/components/Boton_primario.jsx
+++ src/components/Boton_primario.jsx
@@ -7,7 +7,8 @@ import { useNavigate } from "react-router";
 export default function Boton_primario( {text , link}) {
     const navigation = useNavigate();
     return (
-            <button onClick={()=> navigation(link)} className="btn btn-gradient btn-accent ">
+            <button onClick={()=> navigation(link)} className="bg-orange-500 hover:bg-orange-700 active:bg-orange-900 font-bold shadow-xl rounded-xl w-35 h-12 text-amber-50 
+              transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 position:relative">
             {text}
             
             </button> 
