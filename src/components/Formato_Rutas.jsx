1b4af6a896358defd651560a786092b902a2e395
luisma-2601 <142758332+luisma-2601@users.noreply.github.com>
2025-03-18 17:29:23 -0400

    Publicacion de rutas

--- src/components/Formato_Rutas.jsx
+++ src/components/Formato_Rutas.jsx
@@ -4,6 +4,9 @@ import { Link, useNavigate } from 'react-router-dom'
 
 export default function Formato_Rutas({Ruta}) {
     const navigation = useNavigate()
+    const navegar = () => {
+        navigation(`/item/${Ruta.id}`)
+    }
     return(
         <>
         <div className="container mx-auto w-xl transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100 hover:shadow position:relative">
@@ -26,9 +29,9 @@ export default function Formato_Rutas({Ruta}) {
             </p>
             </div>
             <div className='text-right' >
-                <button className="Saber-mas mb-3 mr-3 bg-orange-500 hover:bg-orange-700 active:bg-orange-900 font-bold shadow-xl rounded-xl w-35 h-12 text-amber-50 
+                <button className="Saber-mas mb-3 mr-3 bg-orange-500 hover:bg-orange-700 active:bg-orange-900 font-bold shadow-xl rounded-xl w-20 h-10 text-amber-50 
                     transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 position:relative" 
-                    onClick={navigation("/item/"+Ruta.id)}>Saber Más
+                    onClick={navegar}>Ver Más
                 </button>
             </div>
         </article>
